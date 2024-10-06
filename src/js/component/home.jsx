7dd7677e67e85a38fc8e5dcfec9ecbb813650687
Esteban Brea <83199483+estebanbrea@users.noreply.github.com>
2024-10-06 22:30:00 +0000

    Esteban_Brea-react-hello-simple-counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,27 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-regular-svg-icons";
 
 //create your first component
-const Home = () => {
+const Home = ({seconds}) => {
+
+	let unidades = Math.floor(seconds % 10);
+	let decenas = Math.floor(seconds / 10) % 10;
+	let centenas = Math.floor(seconds / 100) % 10;
+	let milesimas = Math.floor(seconds / 1000) % 10;
+	let decenasDeMiles = Math.floor(seconds / 10000) % 10;
+	let centenasDeMiles = Math.floor(seconds / 100000) % 10;
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className="contador">
+			<div className="digito"><FontAwesomeIcon icon={faClock} /></div>
+			<div className="digito">{centenasDeMiles}</div>
+			<div className="digito">{decenasDeMiles}</div>
+			<div className="digito">{milesimas}</div>
+			<div className="digito">{centenas}</div>
+			<div className="digito">{decenas}</div>
+			<div className="digito">{unidades}</div>
+			</div>
 	);
 };
 
